2fa2a343c863127d17887749f371c767a17fe6e4
George Yiannakas <yiannakas.george@gmail.com>
2019-10-27 15:42:24 +0200

    Fixes: relative paths update so that images are served correctly in production build and node server

--- config/webpack.common.config.js
+++ config/webpack.common.config.js
@@ -6,74 +6,81 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require("webpack");
 
 module.exports = {
-  entry: './src/app.js',
-  output: {
-    filename: '[name].[chunkhash].js',
-    path: path.resolve(__dirname, '../dist')
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-        title: 'Wholegrain',
-        template: './src/index.html',
-        inject: true,
-        minify: {
-            removeComments: true,
-            collapseWhitespace: false
-        }
-    }),
-    new MiniCssExtractPlugin({
-        filename: 'style.[chunkhash].css'
-    }),
-    new CopyWebpackPlugin([{
-        from:'./src/assets/images',      
-        to:'assets/images'   
-    }]),
-    new CleanWebpackPlugin(),
-    new webpack.ProvidePlugin({
-        $: "jquery",
-        jQuery: "jquery"
-    })
-  ],
-  module: {
-    rules: [
-      {
-          test: [/.js$/],
-          exclude: /(node_modules)/,
-          use: {
-              loader: 'babel-loader',
-              options: {
-                  presets: [
-                      '@babel/preset-env'
-                  ]
-              }
-          }
-      },
-      {
-        test: [/.css$|.scss$/],   
-        use: [MiniCssExtractPlugin.loader, 'css-loader','sass-loader'],
-      },
-      {
-        test: /\.(png|jpg|gif)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'assets/images'
+    entry: './src/app.js',
+    output: {
+        filename: '[name].[chunkhash].js',
+        path: path.resolve(__dirname, '../dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Wholegrain',
+            template: './src/index.html',
+            inject: true,
+            minify: {
+                removeComments: false,
+                collapseWhitespace: false
             }
-          }
-        ]
-      },
-      {
-        test: /\.(woff(2)?|ttf|eot|svg|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'file-loader',
-        options: {
-            name: 'fonts/[name].[ext]?[hash]',
-            publicPath: 'assets/fonts'
-        }
-       }
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style.[chunkhash].css'
+        }),
+        new CopyWebpackPlugin([{
+            from: './src/assets/images',
+            to: 'assets/images'
+        }]),
+        new CleanWebpackPlugin(),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        })
     ],
-  },
-  resolve: {extensions: ['.js', '.ts'] }
-  
+    module: {
+        rules: [{
+                test: [/.js$/],
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env'
+                        ]
+                    }
+                }
+            },
+            {
+                test: [/.css$|.scss$/],
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+            // {
+            //     test: /\.(png|jpg|gif)$/,
+            //     use: [{
+            //         loader: 'file-loader',
+            //         options: {
+            //             name: '[name].[ext]',
+            //             outputPath: 'assets/images'
+            //         }
+            //     }]
+            // },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'file-loader',
+                options: {
+                    name: 'fonts/[name].[ext]?[hash]',
+                    publicPath: 'assets/fonts'
+                }
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 1000,
+                        name: 'assets/images/[name].[ext]',
+                    }
+                }]
+            }
+        ],
+    },
+    resolve: { extensions: ['.js', '.ts'] }
+
 };
